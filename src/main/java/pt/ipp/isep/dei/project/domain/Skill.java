afa9c408083d27293114e597a253707eb5ad0d7e
Tchyks <1231235@isep.ipp.pt>
2024-05-01 15:29:10 +0100

    LFC2-127 us001 Java code

--- src/main/java/pt/ipp/isep/dei/project/domain/Skill.java
+++ src/main/java/pt/ipp/isep/dei/project/domain/Skill.java
@@ -1,4 +1,32 @@
 package pt.ipp.isep.dei.project.domain;
 
+import java.util.regex.Pattern;
+
 public class Skill {
+    private String designation;
+
+    public Skill(String designation) {
+        setDesignation(designation);
+    }
+
+    public void setDesignation(String designation) {
+        if (!verifyFilled(designation) || !verifySpecialCharacters(designation)) {
+            this.designation = designation;
+        } else {
+            throw new IllegalArgumentException("The skill designation is invalid!");
+        }
+    }
+
+    private boolean verifySpecialCharacters(String designation) {
+        String regex = "^[a-zA-Z0-9 ]+$";
+        return Pattern.matches(regex, designation);
+    }
+
+    private boolean verifyFilled(String title) {
+        return title.trim().isEmpty();
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
 }
