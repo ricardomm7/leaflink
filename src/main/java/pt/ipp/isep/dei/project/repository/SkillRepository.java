7d78a9daeb09ec4a4a9210586fa8b782742dfca2
Tchyks <1231235@isep.ipp.pt>
2024-05-01 16:05:06 +0100

    LFC2-127 us001 Java code - skill repository

--- src/main/java/pt/ipp/isep/dei/project/repository/SkillRepository.java
+++ src/main/java/pt/ipp/isep/dei/project/repository/SkillRepository.java
@@ -1,4 +1,40 @@
 package pt.ipp.isep.dei.project.repository;
 
+import pt.ipp.isep.dei.project.domain.Job;
+import pt.ipp.isep.dei.project.domain.Skill;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SkillRepository {
+
+    private final List<Skill> skillList;
+
+    public SkillRepository() {
+        skillList = new ArrayList<>();
+    }
+
+    public void createSkill (String designation){
+        Skill skill = new Skill(designation);
+        if (checkForDuplicates(skill)) {
+            addSkill(skill);
+        }
+    }
+
+    private boolean checkForDuplicates(Skill skill) {
+        for (Skill x : skillList) {
+            if (x.getDesignation().equalsIgnoreCase(skill.getDesignation())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void addSkill(Skill skill) {
+        skillList.add(skill);
+    }
+
+    public List<Skill> getSkillList() {
+        return new ArrayList<>(skillList);
+    }
 }
