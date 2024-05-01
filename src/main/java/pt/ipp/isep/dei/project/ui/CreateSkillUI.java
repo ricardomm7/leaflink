c45695c02c9f0c24e2f38f77837ba9888526ade3
Tchyks <1231235@isep.ipp.pt>
2024-05-01 22:31:04 +0100

    LFC2-127 us001 Java Code - CreateSkillUI

---
+++ src/main/java/pt/ipp/isep/dei/project/ui/CreateSkillUI.java
@@ -0,0 +1,28 @@
+package pt.ipp.isep.dei.project.ui;
+
+import pt.ipp.isep.dei.project.application.controller.CreateSkillController;
+
+import java.util.Scanner;
+
+public class CreateSkillUI implements Runnable {
+
+    private static Scanner sc = new Scanner(System.in);
+
+    public void createNewSkill(){
+        CreateSkillController jc = new CreateSkillController();
+        System.out.println("What is the skill designation");
+        String designation = sc.nextLine();
+
+        System.out.println("You want to create a skill with the name " + designation + "? (Y/N)");
+        String decis = sc.nextLine();
+        if (decis.trim().equals("Y")) {
+            jc.createSkill(designation);
+            System.out.println("Operation successfully completed!");
+        } else {
+            System.out.println("Operation cancelled!");
+        }
+    }
+    public void run(){
+        createNewSkill();
+    }
+}
