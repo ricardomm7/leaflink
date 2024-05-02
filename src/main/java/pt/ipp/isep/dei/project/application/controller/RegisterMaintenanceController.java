b119a1a799cd57027d6d965e131fe52c55af0a04
Tchyks <1231235@isep.ipp.pt>
2024-05-02 19:25:13 +0100

    Create RegisterMaintenanceController.java

---
+++ src/main/java/pt/ipp/isep/dei/project/application/controller/RegisterMaintenanceController.java
@@ -0,0 +1,32 @@
+package pt.ipp.isep.dei.project.application.controller;
+
+import pt.ipp.isep.dei.project.domain.Job;
+import pt.ipp.isep.dei.project.repository.CollaboratorRepository;
+import pt.ipp.isep.dei.project.repository.JobRepository;
+import pt.ipp.isep.dei.project.repository.Repositories;
+
+import java.util.Date;
+import java.util.List;
+
+public class RegisterMaintenanceController {
+    private final CollaboratorRepository collaboratorRepository;
+    private final JobRepository jobRepository;
+
+
+    public RegisterMaintenanceController() {
+        Repositories repositories = Repositories.getInstance();
+        collaboratorRepository = repositories.getCollaboratorRepository();
+        jobRepository = repositories.getJobRepository();
+    }
+
+    public void createCLB(String name, Date birthdate, int contactMobile, int taxpayerNumber, String email, String address, String zipCode, String city, String documentType, String identificationNumber, Date admissionDate, Job job) {
+        collaboratorRepository.create(name, birthdate, contactMobile, taxpayerNumber, email, address, zipCode, city, documentType, identificationNumber, admissionDate, job);
+    }
+
+    public List<Job> getJobs() {
+        return jobRepository.getJobList();
+    }
+
+    public void createMNT(String s) {
+    }
+}
