b7bfbada86f5bec90136ad6bcc0a4440cc9c90b9
Ricardo Meireles <1230744@isep.ipp.pt>
2024-05-16 18:46:43 +0100

    QAM menu changes. #118

--- src/main/java/pt/ipp/isep/dei/project/ui/console/menu/QamUI.java
+++ src/main/java/pt/ipp/isep/dei/project/ui/console/menu/QamUI.java
@@ -1,10 +1,6 @@
 package pt.ipp.isep.dei.project.ui.console.menu;
 
-import pt.ipp.isep.dei.project.ui.ListMaintenanceUI;
-import pt.ipp.isep.dei.project.ui.RegisterMaintenanceUI;
-import pt.ipp.isep.dei.project.ui.RegisterVehicleUI;
 import pt.ipp.isep.dei.project.ui.RunTestsUI;
-import pt.ipp.isep.dei.project.ui.console.ShowTextUI;
 import pt.ipp.isep.dei.project.ui.console.utils.Utils;
 
 import java.util.ArrayList;
@@ -16,7 +12,7 @@ public class QamUI implements Runnable {
 
     public void run() {
         List<MenuItem> options = new ArrayList<MenuItem>();
-        options.add(new MenuItem("Run Tests", new RunTestsUI()));
+        options.add(new MenuItem("Run Tests for time about Kruskal", new RunTestsUI()));
 
         int option = 0;
         do {
