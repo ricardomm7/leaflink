4172a8a9861373a53ab96f5a2bd60617816467ba
Joana Gama <1230399@isep.ipp.pt>
2024-06-07 15:48:15 +0100

    Added JavaDoc on some classes

--- src/main/java/pt/ipp/isep/dei/project/application/controller/AddAgendaEntryController.java
+++ src/main/java/pt/ipp/isep/dei/project/application/controller/AddAgendaEntryController.java
@@ -12,31 +12,55 @@ import pt.ipp.isep.dei.project.repository.Repositories;
 
 import java.util.List;
 
+
+/**
+ * Controller class responsible for adding and managing agenda entries.
+ */
 public class AddAgendaEntryController {
     private final Repositories repositories;
     private final EntryRepository entryRepository;
 
+    /**
+     * Constructs an instance of AddAgendaEntryController.
+     * Initializes the repositories and entry repository.
+     */
     public AddAgendaEntryController() {
         repositories = Repositories.getInstance();
         entryRepository = repositories.getEntryRepository();
     }
 
+    /**
+     * Retrieves the list of ToDo entries as DTOs.
+     *
+     * @return a list of ToDoEntryDto representing the ToDo entries.
+     */
     public List<ToDoEntryDto> getToDoEntry() {
         return ToDoEntryMapper.toDtoList(entryRepository.getToDoEntryList());
     }
 
+    /**
+     * Adds a new agenda entry.
+     * Converts the provided DTO to a domain object and adds it to the repository.
+     * Also removes the corresponding ToDo entry from the repository if it exists.
+     *
+     * @param entry the AgendaEntryDto to be added.
+     */
     public void addAgendaEntry(AgendaEntryDto entry) {
         AgendaEntry agendaEntry = AgendaEntryMapper.toDomain(entry);
         entryRepository.addAgendaEntry(agendaEntry);
 
-        // Remove corresponding ToDoEntry from the repository
         ToDoEntry toDoEntry = entryRepository.findToDoEntryByTitle(entry.getTitle());
         if (toDoEntry != null) {
             entryRepository.removeToDoEntry(toDoEntry);
         }
     }
 
-
+    /**
+     * Retrieves the list of agenda entries for a specific user session.
+     *
+     * @param GSM the UserSession representing the user.
+     * @return a list of AgendaEntryDto representing the agenda entries for the user.
+     */
     public List<AgendaEntryDto> getAgendaEntries(UserSession GSM) {
         return AgendaEntryMapper.toDtoList(entryRepository.getAgendaEntryListByGSM(GSM.getUserEmail()));
     }
